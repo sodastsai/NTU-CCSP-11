266a970d3e0a1fbf5f0c58b644a52649051bea29
sodas <sodas2002@gmail.com>
2011-03-18 00:28:08 +0800

    Finish basic board

--- CCSP_HW1/src/MainHandler.py
+++ CCSP_HW1/src/MainHandler.py
@@ -2,12 +2,19 @@ from google.appengine.dist import use_library
 use_library('django', '1.2')
 
 from google.appengine.api import users
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 import os
 
+## Timezone filter
+template.register_template_library('twtz.DjangoTwtz')
+
+class Message(db.Model):
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp.RequestHandler):    
     def get(self):
@@ -18,12 +25,33 @@ class MainPage(webapp.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
             return
         
+        # Messages
+        messages = Message.all().order('-date').fetch(10)
+        
         # Output
-        templateDict = { "logout": users.create_logout_url("http://www.google.com/") }
+        templateDict = {
+                        "logout": users.create_logout_url(self.request.uri),
+                        "username": user.nickname(),
+                        "messages": messages
+                        }
         indexPath = os.path.join(os.path.dirname(__file__), "index.html")
         self.response.out.write(template.render(indexPath, templateDict))
+        
+class Guestbook(webapp.RequestHandler):
+    def post(self):
+        msg = Message()
+        if users.get_current_user():
+            msg.author = users.get_current_user()
+        
+        msg.content = self.request.get('content')
+        msg.put()
+        self.redirect('/')
+        
+class NotFound(webapp.RequestHandler):
+    def get(self):
+        self.redirect("/")
 
-application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+application = webapp.WSGIApplication([('/', MainPage), ('/send', Guestbook), ('/.*', NotFound)], debug=True)
 
 def main():
     run_wsgi_app(application)
