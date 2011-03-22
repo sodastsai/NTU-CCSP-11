fe26e0e40716228ebb5de7088bcc2c18759310c9
sodas <sodas2002@gmail.com>
2011-03-23 03:19:54 +0800

    Add comment

--- CCSP_HW1/src/keep/index.js
+++ CCSP_HW1/src/keep/index.js
@@ -1,37 +1,46 @@
+// Show information in information bar
+// Data is come from webservice api
 function showInfo(data) {
-	// Date
+	// today's date
 	var date = new Date();
 	$("#date").html(date.toDateString());
-	// Weather
+	// Weather information
 	weatherStr = data["city"]+" "+data["temperature"]+"&#176;C "+data["description"];
 	$("#weather").html(weatherStr);
 }
 
-// Delete Button
+// Delete Button as jQuery plugin
 $.fn.deleteBtn = function() {
     return this.each(function() {
     	$(this).click(function(){
+    		// Click action
+    		// Show loading hint
     		$("#loading").slideDown();
+    		// Call AJAX for delete message
     		$.ajax({
     			url: "/delete",
     			type: "post",
     			data: {key: $(this).attr("id") },
     			dataType: "html",
     			success: function(response) {
+    				// Close loading hint
     				$("#loading").slideUp();
+    				// Hide message deleted
     				$("#post_"+response).slideUp();
     			}
     		});
     	}).hover(function(){
+    		// Mouse Effect
     		$(this).addClass("hover");
     	}, function(){
+    		// Mouse Effect
     		$(this).removeClass("hover");
     	});
     });
 };
 
 $(document).ready(function(){
-	// Check Browser
+	// Check Browser and make them different
 	if (navigator.userAgent.match("Chrome")) {
 		$("div#info").addClass("chrome");
 	} else if (navigator.userAgent.match("Safari")) {
@@ -43,19 +52,25 @@ $(document).ready(function(){
 	} else if (navigator.userAgent.match("MSIE")) {
 		$("div#info").addClass("msie");
 	}
-	// Delete Button
+	
+	// Delete Button activation
 	$(".postDelete").deleteBtn();
-	// Load info
+	
+	// Load information bar with JSONP
 	$.ajax({
 		url: "http://ntu-taiwan-weather.appspot.com/json/current/Taipei/?callback=showInfo",
 		dataType: "jsonp"
 	});
-	// Submit Button
+	
+	// Submit Button activation
 	$("#content input[type='submit']").hover(function(){
+		// Mouse Effect
 		$(this).addClass("hover");
 	}, function(){
+		// Mouse Effect
 		$(this).removeClass("hover");
 	}).click(function(){
+		// Show loading hint
 		$("#loading").slideDown();
 		// AJAX submit and return
 		$.ajax({
@@ -64,17 +79,22 @@ $(document).ready(function(){
 			data: { content: $("textarea[name='content']").val() },
 			dataType: "json",
 			success: function(response) {
-				// Clean
+				// Clean input
 				$("textarea[name='content']").val("");
+				// Close loading hint
 				$("#loading").slideUp();
-				// Show
+				// Show data from AJAX Result
+				// New a message tag
 				$("#board").prepend("<div class=\"post\"></div>");
+				// Make basic structure
 				$("#board div.post:first").append("<div class=\"postBody\" id=\"post_"+response["key"]+"\"></div>").hide();
 				$("#board div.post:first").append("<div><span class=\"postAuthor\"></span><span class=\"postTime\"></span><span class=\"postDelete\">erase</span></div>");
+				// Fill data and activate action
 				$("#board div.post:first .postBody").html(response["content"]);
 				$("#board div.post:first .postAuthor").html(response["author"]);
 				$("#board div.post:first .postTime").html(response["date"]);
 				$("#board div.post:first .postDelete").attr("id",response["key"]).deleteBtn();
+				// Show the new message
 				$("#board div.post:first").slideDown();
 			}
 		});
